6d5e7e1131ef85c03e0fdc01cb5e4e46ec4bdb41
w00ry <mehmetsahinku@gmail.com>
2024-07-23 16:54:58 +0300

    Ders1 : Giris - Kurulum - Degiskenler - Operatorler ( C# )
    ders 1
    
    Co-Authored-By: w00ry <174250897+w00ry@users.noreply.github.com>
    Co-Authored-By: Ayhan Akkaya <ayhanakkayameb@gmail.com>

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FULL_STACK_WEB_LESSON1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int yas=18;
+        string AdSoyad=("Mehmet Şahin KURT");
+        double boy=1.76;
+        char Cinsiyet='E';
+        Console.WriteLine(yas);
+        Console.WriteLine(AdSoyad);
+        Console.WriteLine(boy);
+        Console.WriteLine(Cinsiyet);
+    }
+}
